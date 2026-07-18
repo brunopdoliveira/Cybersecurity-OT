2456a3d96a2b7297dad4ffe1bac3239715a890d7
Bruno Oliveira <69878902+brunopdoliveira@users.noreply.github.com>
2026-07-18 19:24:13 -0300

    Update inicio.sh

--- NetLabDocker/netlab/conf/dns/inicio.sh
+++ NetLabDocker/netlab/conf/dns/inicio.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-# para Alpine Linux
-# https://wiki.alpinelinux.org/wiki/Setting_up_a_ssh-server
-# Rodar da primeira vez:
-#ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-#ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-# criando usuario inf500 com senha inf500
-#adduser -D --home /home/inf500 --shell /bin/ash -g "usuario para teste inf500" inf500
-#echo "inf500:inf500" |chpasswd
-# iniciando ssh server
-#/usr/sbin/sshd 
+# Rota IPv4 para a rede dos clientes
+ip route add 192.168.10.0/24 via 100.100.100.254
 
+# Rota IPv6 para a rede dos clientes
+ip -6 route add 2001:db8:2026:10::/64 via 2001:db8:2026:100::254
 
-# Para Debian
-# criando usuario inf500 com senha inf500
-#adduser --home /home/inf500 --shell /bin/bash --disabled-password --gecos "usuario para teste inf500" inf500
-
-# mudando a senha do usuario inf500 para inf500
-#echo "inf500:inf500" |chpasswd
-
-# iniciando ssh server
-#/usr/bin/ssh-keygen -A
-#/etc/init.d/ssh  start
-
-# Removendo rota default 
-#ip route del default
-#ip -6 route del default
-
-# ajustando rota
-ip route add 10.10.0.0/16 via 10.10.100.100 
-ip route add 2001:db8:2021:10::/64 via 2001:db8:2021:100::100 
-ip route add 2001:db8:2021:50::/64 via 2001:db8:2021:100::100 
-
-# inicializando o bind daemon
+# Inicializando o BIND
 /usr/sbin/rndc-confgen -a
 /usr/sbin/named -c /etc/bind/named.conf
 
