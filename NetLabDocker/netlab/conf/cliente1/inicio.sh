1cb78300635cd0f074fa661260e4e8677ab7fc3f
Bruno Oliveira <69878902+brunopdoliveira@users.noreply.github.com>
2026-07-18 18:50:42 -0300

    Update inicio.sh

--- NetLabDocker/netlab/conf/cliente1/inicio.sh
+++ NetLabDocker/netlab/conf/cliente1/inicio.sh
@@ -3,7 +3,7 @@
 ip route del default
 ip -6 route del default
 
-ip route add 10.10.0.0/16 via 10.10.10.100 
-ip route add 2001:db8:2021:100::/64 via 2001:db8:2021:10::100
+ip route add 100.100.100.0/24 via 192.168.10.254
+ip route add 2001:db8:2026:100::/64 via 2001:db8:2026:10::254
 
 while true; do sleep 10; done
